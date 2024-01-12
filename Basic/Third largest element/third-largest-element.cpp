594d13495954fc8eff4458ca10ca6aaa90209b9c
shanmukhnaidu <98636191+shanmukhnaidu@users.noreply.github.com>
2024-01-12 23:12:33 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Third largest element/third-largest-element.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+  public:
+    int thirdLargest(int a[], int n)
+    {
+         //Your code here
+         vector<int>v;
+         for(int i=0;i<n;i++)
+         {
+             v.push_back(a[i]);
+         }
+         sort(v.begin(),v.end());
+         return v[v.size()-3];
+    }
+
+};
+
+//{ Driver Code Starts.
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	        cin>>a[i];
+	   Solution obj;
+	    cout<<obj.thirdLargest(a,n)<<endl;
+    }
+}   
+// } Driver Code Ends

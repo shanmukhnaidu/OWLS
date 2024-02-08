92211f968add351a460068970a3d35adbecc0ddd
shanmukhnaidu <98636191+shanmukhnaidu@users.noreply.github.com>
2024-02-08 23:52:30 +0530

    Time: 4 ms (44.20%) | Memory: 8.8 MB (30.69%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,35 +1,23 @@
 class Solution {
 public:
-    void F(int n,vector<int>&nums,vector<int>&v,vector<vector<int>>&v1,int arr[])
+    void F(int ind,int n,vector<int>&nums,vector<vector<int>> &v1)
     {
-        if(v.size()==n)
-        {
-            v1.push_back(v);
+        if(ind==n){
+            v1.push_back(nums);
             return;
-        }
-        for(int i=0;i<n;i++)
+        } 
+        for(int i=ind;i<n;i++)
         {
-            if(!arr[i])
-            {
-                v.push_back(nums[i]);
-                arr[i]=1;
-                F(n,nums,v,v1,arr);
-                arr[i]=0;
-                v.pop_back();
-            }
+            swap(nums[ind],nums[i]);
+            F(ind+1,n,nums,v1);
+            swap(nums[ind],nums[i]);
         }
     }
 
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<int>v;
-        int n=nums.size();
-        int arr[n];
-        for(int i=0;i<n;i++)
-        {
-            arr[i]=0;
-        }
-        vector<vector<int>>v1;
-        F(n,nums,v,v1,arr);
-        return v1;
+       vector<vector<int>>v1;
+       int n=nums.size();
+       F(0,n,nums,v1);
+       return v1;
     }
 };

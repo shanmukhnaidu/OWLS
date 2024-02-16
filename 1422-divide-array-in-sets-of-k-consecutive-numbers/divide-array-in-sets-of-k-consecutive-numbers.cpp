4d04c8ec05160af4a24703d28582b4ecfa259179
shanmukhnaidu <98636191+shanmukhnaidu@users.noreply.github.com>
2024-02-16 22:26:15 +0530

    Time: 172 ms (27.87%) | Memory: 53.4 MB (34.48%) - LeetSync

--- 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.cpp
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     bool isPossibleDivide(vector<int>& nums, int k) {
-        if(nums.size() % k != 0) return false;
-        map<int,int>mpp;
-        for(auto i : nums) mpp[i]++;
-        for(auto it = mpp.begin(); it != mpp.end();){
-            if(it->second > 0){
-                for(int i =0; i < k; i++){
-                    if(mpp[it->first+i] > 0)
-                        mpp[it->first+i]--;
+        if(nums.size()%k!=0) return false;
+        int s=0;
+        // sort(v.begin(),v.end());
+        map<int,int>mp;
+        for(auto it:nums)
+        {
+            mp[it]++;
+        }
+        // sort(mp.begin(),mp.end());
+        for(auto it=mp.begin();it!=mp.end();)
+        {
+            if(it->second>0)
+            {
+                for(int i=0;i<k;i++)
+                {
+                    if(mp[it->first+i]>0) mp[it->first+i]--;
                     else return false;
                 }
             }
             else it++;
         }
         return true;
-        
     }
 };

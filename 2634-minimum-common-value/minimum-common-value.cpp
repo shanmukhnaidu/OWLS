daa813b65b699c716d0891c8f9ec1748dc2e4bfa
shanmukhnaidu <98636191+shanmukhnaidu@users.noreply.github.com>
2024-03-09 17:31:52 +0530

    Time: 69 ms (66.06%) | Memory: 53 MB (51.54%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -17,7 +17,7 @@ public:
         // sort(nums2.begin(),nums2.end());
         int i=0;
         int j=0;
-        while(i<nums1.size() and j<nums2.size())
+        while(i!=nums1.size() and j!=nums2.size())
         {
             if(nums1[i]==nums2[j]) return nums1[i];
             if(nums1[i]<nums2[j]) i++;

bc8cd233aa79088e6d28a173aac5cc56bbfec2a2
shanmukhnaidu <98636191+shanmukhnaidu@users.noreply.github.com>
2024-03-24 23:09:51 +0530

    Time: 298 ms (5.02%) | Memory: 105.8 MB (6.40%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -14,6 +14,18 @@ public:
                 return it.first;
             }
         }
-        return -1;
+        int z=0;
+            for(int i=1;i<=n;i++)
+                {
+                if(n%i==0)
+                {
+                    z++;
+                }
+                }
+                if(z!=2)
+                {
+                    return false;
+                }
+                return true;
     }
 };

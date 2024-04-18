5cee21170644b20177db5af7bdd94e7f6e121028
shanmukhnaidu <98636191+shanmukhnaidu@users.noreply.github.com>
2024-04-18 16:48:33 +0530

    Updated Solution - GfG to GitHub

--- Medium/Two Repeated Elements/two-repeated-elements.cpp
+++ Medium/Two Repeated Elements/two-repeated-elements.cpp
@@ -11,14 +11,14 @@ using namespace std;
 class Solution {
   public:
     //Function to find two repeated elements.
-    vector<int> twoRepeated (int arr[], int N) {
+    vector<int> twoRepeated (int arr[], int n) {
         // Your code here
-        unordered_map<int,int>mp;
+        map<int,int>mp;
         vector<int>v;
-        for(int i=0;i<N+2;i++)
+        for(int i=0;i<n+2;i++)
         {
             mp[arr[i]]++;
-            if(mp[arr[i]]==2) v.push_back(arr[i]);
+            if(mp[arr[i]]>1) v.push_back(arr[i]);
         }
         return v;
     }

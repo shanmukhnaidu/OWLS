fc4055ef6913d0fbfcb85b7073169735ad95758f
shanmukhnaidu <98636191+shanmukhnaidu@users.noreply.github.com>
2024-05-10 10:22:20 +0530

    Time: 459 ms (30.56%) | Memory: 120.7 MB (28.63%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
-        priority_queue<pair<double, pair<int, int>>, vector<pair<double, pair<int, int>>>, greater<>> minHeap;
-        int N = arr.size();
-        for (int i = 0; i < N; ++i) {
-            for (int j = i + 1; j < N; ++j) {
-                minHeap.push({(double)arr[i] / arr[j], {arr[i], arr[j]}});
+        // map<double,pair<int,int>>mp;
+        priority_queue<pair<double,pair<int,int>>,vector<pair<double,pair<int,int>>>,greater<>>q;
+        int n=arr.size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                q.push({(double)arr[i]/arr[j],{arr[i],arr[j]}});
             }
         }
-        
-        pair<int, int> result;
-        for (int i = 0; i < k; ++i) {
-            result = minHeap.top().second;
-            minHeap.pop();
+        pair<int,int>ans;
+        for(int i=0;i<k;i++)
+        {
+            ans=q.top().second;
+            q.pop();
         }
-        
-        return {result.first, result.second};
+        return {ans.first,ans.second};
     }
 };
